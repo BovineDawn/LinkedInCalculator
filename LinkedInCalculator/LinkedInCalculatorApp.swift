570aeb0a2fcfdcb807649c881969142efe24a242
Justin <109449719+BovineDawn@users.noreply.github.com>
2023-02-27 19:26:20 -0700

    Initial Commit

---
+++ LinkedInCalculator/LinkedInCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInCalculatorApp.swift
+//  LinkedInCalculator
+//
+//  Created by JG on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
